62678927b23a112767a42ddb7498ca243abe82f8
byoungjo <ishtar1313!!>
2021-05-16 05:11:21 +0900

    dd

--- src/test/java/chap01/CalculatorTest.java
+++ src/test/java/chap01/CalculatorTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-    public CalculatorTest() {
-    }
 
     @Test
-    static void plus(){
+    void plus(){
         int result = Calcluator.plus(1,2);
         assertEquals(3, result);
+        assertEquals(5, Calcluator.plus(4,1));
     }
 }
